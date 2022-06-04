624a784a892f43f84cdb3b3f156adc60cedccd68
Orkusha <orkusha.d@gmail.com>
2022-06-04 23:23:26 +0300

    Initial Commit

---
+++ RGBullsEys/RGBullsEysApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEysApp.swift
+//  RGBullsEys
+//
+//  Created by mac on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
