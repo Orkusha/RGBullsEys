7cb55d46d1d64320d74aab5cb24d8f8ea5640fb0
Orkusha <orkusha.d@gmail.com>
2022-06-05 01:39:44 +0300

    add color, button and slider

--- RGBullsEys/ContentView.swift
+++ RGBullsEys/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var game = Game()
+    @State var guess: RGB
+    var target = RGB.random()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+            Text("R: ???, G: ???, B: ???")
+                .padding()
+            Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+            Text("R: 204, G: 76, B: 178")
+                .padding()
+            Slider(value: .constant(0.5))
+            Button("Hit Me!") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+        }
     }
 }
 
